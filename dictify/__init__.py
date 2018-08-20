c25e679013bfe1fa92166404dd72740b84e8abe2
Nitipit Nontasuwan <nitipit@gmail.com>
2018-08-20 09:48:20 +0700

    update

--- dictify/__init__.py
+++ dictify/__init__.py
@@ -1,75 +1,73 @@
 import re
 
 
-class Query:
-    def __init__(self, field, verify):
+class Verify:
+    def __init__(self, field, func):
         self._field = field
-        self.verify = verify
-        self._field.queries.append(self)
+        self.func = func
 
     def __call__(self, *args, **kw):
-        self.verify(self._field, *args, **kw)
+        self.func(self._field)
+
+    def field(func):
+        """Decorate function to append verification to `self.queries`."""
+        def wrapper(self, *args, **kw):
+            verify = Verify(self, func)
+            self.verifies.append(verify)
+            return self
+        return wrapper
 
 
 class Field:
     """Class contain function to verify Dictify Field.
 
     ## Example:
-    class Person(Dictify):
+    class User(Model):
         name = Field().required()
     """
 
     def __init__(self):
         """Initialize function for Field()."""
-        self.queries = []
+        self.verifies = []
         self.value = None
 
-    def verify(func):
-        """Decorate function to append verification to `self.queries`."""
-        def wrapper(self, *args, **kw):
-            query = Query(self, func)
-            # self.queries.append(call)
-            return self
-        return wrapper
-
     def _verify(self, key, value):
-        self.key = key
         self.value = value
         self.errors = list()
-        for query in self.queries:
+        for verify in self.verifies:
             try:
-                query()
+                verify()
             except ValueError as e:
                 self.errors.append(e.args[0])
         if self.errors:
-            raise ValueError("'%s' : %s" % (self.key, self.errors))
+            raise ValueError("'%s' : %s" % (key, self.errors))
         return self.value
 
-    @verify
+    @Verify.field
     def required(self):
         """Require value."""
         if (self.value is None) or (self.value == ''):
             raise ValueError('Required')
 
-    @verify
+    @Verify.field
     def default(self, default):
         """Set default value."""
         if self.value is None:
             self.value = default
 
-    @verify
+    @Verify.field
     def type(self, classinfo):
         """Check value type."""
         if not(isinstance(self.value, classinfo)):
             raise ValueError('Must be %s object' % classinfo)
 
-    @verify
+    @Verify.field
     def match(self, re_):
         """Apply re.match to value."""
         if not re.match(re_, self.value):
             raise ValueError("Value not match with '%s'" % re_)
 
-    @verify
+    @Verify.field
     def apply(self, func):
         """Apply function to Field(). Receive `self` as first args.
 
@@ -92,6 +90,10 @@ class Model(dict):
         def __setitem__(self, k, v):
             v = self._field[k]._verify(k, v)
             return super().__setitem__(k, v)
+
+        @property
+        def field(self):
+            return self._field
 
     def __new__(cls, dict_=dict()):
         cls._field = dict()
