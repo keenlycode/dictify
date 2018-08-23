1092c8b202f3d617b8e2b0047fa32be8c2933fd7
Nitipit Nontasuwan <nitipit@gmail.com>
2018-08-24 03:04:19 +0700

    v0.1.1 - Handle KeyError Exception.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dictify",
-    version="0.1.1dev",
+    version="0.1.1",
     author="Nitipit Nontasuwan",
     author_email="nitipit@gmail.com",
     description="Python `dict` and `json` verification for mankind :)",
