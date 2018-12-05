437904a4f393ba383086725ab3e73606b85203ac
Nitipit Nontasuwan <nitipit@gmail.com>
2018-12-05 16:55:14 +0700

    update

--- docs-src/docs.py
+++ docs-src/docs.py
@@ -1,12 +1,20 @@
+import os
+
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 env = Environment(
     loader=FileSystemLoader('template'),
     autoescape=select_autoescape(['html', 'xml']),
     trim_blocks=True,
 )
-
 template = env.get_template('index.html')
+src_dir = os.path.dirname(os.path.abspath(__file__))
 
-f = open('index.html', 'w')
+dest_dir = os.path.join(src_dir, '../docs/')
+dest_dir = os.path.dirname(os.path.abspath(dest_dir))
+print(dest_dir)
+if not os.path.exists(dest_dir):
+    os.makedirs(dest_dir)
+
+f = open('../docs/index.html', 'w')
 f.write(template.render())
 f.close()
--- docs-src/gulpfile.js
+++ docs-src/gulpfile.js
@@ -1,8 +1,16 @@
 const gulp = require('gulp');
 const stylus = require('gulp-stylus');
 
-function stylus() {
+function bits_ui() {
+    return gulp.src('bits-ui/bits-ui.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('../docs/bits-ui/'));
+};
 
-}
+function template_stylus() {
+    return gulp.src('template/**/*.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('../docs/'));
+};
 
-exports.default = gulp.series(stylus)
+exports.default = gulp.series(bits_ui, template_stylus);
