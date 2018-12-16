49cf556f52327fd795b8ff45451d0ecd87e9bf1e
Nitipit Nontasuwan <nitipit@gmail.com>
2018-12-17 01:44:44 +0700

    v1.1.1 - refactor code - support subclass

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dictify",
-    version="1.1.1dev",
+    version="1.1.1",
     author="Nitipit Nontasuwan",
     author_email="nitipit@gmail.com",
     description="Python `dict` and `json` verification for humankind :)",
