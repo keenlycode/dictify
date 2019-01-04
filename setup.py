d69bef9461a56913f3eac8889349577d918c0920
Nitipit Nontasuwan <nitipit@gmail.com>
2019-01-04 15:01:24 +0700

    v1.1.2 fix issue-6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dictify",
-    version="1.1.1",
+    version="1.1.2",
     author="Nitipit Nontasuwan",
     author_email="nitipit@gmail.com",
     description="Python `dict` and `json` verification for humankind :)",
