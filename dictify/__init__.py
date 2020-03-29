a76c2413c8c9a949e2cbbd42bac990bfae4d2e9f
= <nitipit@gmail.com>
2020-03-29 17:23:10 +0700

    update

--- dictify/__init__.py
+++ dictify/__init__.py
@@ -1,256 +1,183 @@
-import re
 import math
-import unittest
-import warnings
-
-test_case = unittest.TestCase()
+import re
 
 
-class Function:
-    def __init__(self, func, *args, **kw):
-        """Keep `func`, `*args` and `**kw` to be called."""
-        self.func = func
-        self.args = args
-        self.kw = kw
+def rule(func):
+    """Decorator to add function to field for validation."""
+    def wrapper(field, *args, **kw):
+        field._functions.append(lambda field: func(field, *args, **kw))
+        return field
+    return wrapper
 
-    def __call__(self, value):
-        return self.func(value, *self.args, **self.kw)
 
-class define:
-    @staticmethod
-    def value(func):
-        """Decorator to add function to field if value is provided."""
+class ModelError(Exception):
+    pass
 
-        def wrapper(field, *args, **kw):
-            def f(value, *args, **kw):
-                if value is None or '' or []:
-                    return value
-                else:
-                    return func(value, *args, **kw)
-            function = Function(f, *args, **kw)
-            field.functions.append(function)
-            return field
-        return wrapper
 
-    @staticmethod
-    def empty(func):
-        """Decorator to add field's function if value is empty"""
-        def wrapper(field, *args, **kw):
-            def f(value, *args, **kw):
-                if value is None or '' or []:
-                    return func(value, *args, **kw)
-                else:
-                    return value
-            function = Function(f, *args, **kw)
-            field.functions.append(function)
-            return field
-        return wrapper
+class FieldError(Exception):
+    pass
 
-    @staticmethod
-    def any(func):
-        def wrapper(field, *args, **kw):
-            function = Function(func, *args, **kw)
-            field.functions.append(function)
-            return field
-        return wrapper
+
+class ListError(Exception):
+    pass
 
 
 class ListOf(list):
-    def __init__(self, type_=None, values=[]):
+    def __init__(self, field, type_=None):
         self._type = type_
         errors = list()
-        for v in values:
+        for v in field.value:
             try:
-                test_case.assertIsInstance(v, self._type)
+                assert isinstance(v, self._type),\
+                    f"'{v}' is not instance of {self._type}"
             except AssertionError as e:
                 errors.append(e)
         if errors:
-            raise ValueError(errors)
-        super().__init__(values)
+            raise ListError(errors)
+        super().__init__(field.value)
 
     def __setitem__(self, index, value):
-        test_case.assertIsInstance(value, self._type)
+        assert isinstance(value, self._type),\
+            f"'{value}' is not instance of {self._type}"
         return super().__setitem__(index, value)
 
     def append(self, value):
-        test_case.assertIsInstance(value, self._type)
+        assert isinstance(value, self._type),\
+            f"'{value}' is not instance of {self._type}"
         return super().append(value)
 
 
 class Field:
+    def __init__(self, required=False, default=None):
+        self._required = required
+        self._default = default
+        self._value = default
+        self._functions = list()
 
-    def __init__(self):
-        self.value = None
-        self.functions = []
+    @property
+    def value(self):
+        return self._value
 
-    def query(self, value):
+    def _validate(self, value=None):
         """Apply all functions to field's value"""
         errors = list()
-        self.value = value
-        for function in self.functions:
+        if value is not None:
+            self._value = value
+        else:
+            self._value = self._default
+        if self._required:
+            assert self.value is not None, 'Value is required'
+        if self.value is None:
+            return self
+        for function in self._functions:
             try:
-                value = function(self.value)
-                if value is not None:
-                    self.value = value
-            except (ValueError, AssertionError) as e:
-                errors.append(e.args[0])
+                function(self)
+            except AssertionError as e:
+                errors.append(e)
         if errors:
-            raise ValueError(errors)
+            raise FieldError(errors)
         return self
 
-    @define.value
-    def any(value, members: list):
-        """(Deprecated) Check if value is any of members."""
-        warnings.warn('Deprecated. Changed to `anyof`', DeprecationWarning)
-        test_case.assertIn(value, members)
+    @rule
+    def type(self, type_):
+        assert isinstance(self.value, type_), f'Value is not instance of {type_}'
 
-    @define.value
-    def anyof(value, members: list):
+    @rule
+    def anyof(self, members: list):
         """Check if value is any of members."""
-        test_case.assertIn(value, members)
+        assert self.value in members, f'{self.value} is not in {members}'
 
-    @define.any
-    def apply(value, func: 'function to apply'):
-        """Apply function to Field().
-
-        ## Example use:
-        class User(Model):
-            def check(value):
-                # Do something with value,
-                # like checking value or setting value.
-
-            name = Field().apply(check)
-        """
-        return func(value)
-
-    @define.empty
-    def default(value, default_):
-        """Set default value."""
-        if callable(default_):
-            return default_()
-        else:
-            return default_
-
-    @define.empty
-    def required(value):
-        """Required."""
-        raise ValueError('Required.')
-
-    @define.value
-    def length(value: (str, list), min: int = 0, max: int = math.inf):
+    @rule
+    def length(self, min: int = 0, max: int = math.inf):
         """Set min/max of value's length."""
-        try:
-            length_ = len(value)
-        except TypeError:
-            raise ValueError('Value must be `list, str` type')
-        if not min <= length_ <= max:
-            raise ValueError(
-                'Value\'s length is %s. Must be %s to %s'
-                % (length_, min, max)
-            )
+        assert isinstance(self.value, (str, list)),\
+            f"Value muse be `str` or `list` object"
+        length_ = len(self.value)
+        assert min <= length_ <= max,\
+            f"Value's length is {length_}. Must be {min} to {max}"
 
-    @define.value
-    def listof(values, type_=None):
-        return ListOf(type_, values)
+    @rule
+    def listof(self, type_):
+        self._value = ListOf(self, type_)
 
-    @define.value
-    def match(value, re_: 'regular expession string'):
+    @rule
+    def verify(self, func):
+        func(self)
+
+    @rule
+    def search(self, re_: str):
         """Check value matching with regular expression."""
-        test_case.assertRegex(value, re_)
+        assert re.search(re_, self.value)
 
-    @define.value
-    def number(value: (int, float, complex),
-               min: (int, float, complex) = -math.inf,
-               max: (int, float, complex) = math.inf):
-        """Check if value is number"""
-        if not min <= value <= max:
-            raise ValueError(
-                'Value is %s, must be %s to %s'
-                % (value, min, max)
-            )
+    @rule
+    def min(self, min_: (int, float, complex) = -math.inf, equal=True):
+        if equal is True:
+            assert self.value >= min_, f"{self.value} => {min_}"
+        else:
+            assert self.value > min_, f"{self.value} > {min_}"
 
-    @define.value
-    def range(value: (int, float, complex),
-              min: (int, float, complex) = -math.inf,
-              max: (int, float, complex) = math.inf):
-        """(Deprecated) Set possible value range."""
-        if not min <= value <= max:
-            raise ValueError(
-                'Value is %s, must be %s to %s'
-                % (value, min, max)
-            )
+    @rule
+    def max(self, max_: (int, float, complex) = -math.inf, equal=True):
+        if equal is True:
+            assert self.value <= max_, f"{self.value} <= {max_}"
+        else:
+            assert self.value < max_, f"{self.value} < {max_}"
 
-    @define.value
-    def subset(values, members: list):
+    @rule
+    def subset(self, members: list):
         """Check if value is subset of defined members."""
-        test_case.assertLessEqual(set(values), set(members), msg="Not a subset")
-
-    @define.value
-    def type(value, type_: type):
-        """Check value's type."""
-        test_case.assertIsInstance(value, type_)
+        assert set(self.value) <= set(members),\
+            f"{self.value} is not a subset of {members}"
 
 
 class Model(dict):
-    """Class to defined fields and rules.
-
-    ## Example:
-    Class User(Model):
-        name = Field().required().type(str)
-
-    user = User({'name': 'John'})
-    """
+    """Class to defined fields and rules."""
 
     def __init__(self, data=dict()):
-        field = dict()
-        errors = dict()
-        result = dict()
-        data = data.copy()
-        for k in self.__dir__():
-            f = self.__getattribute__(k)
-            if isinstance(f, Field):
-                try:
-                    field[k] = f.query(data.get(k))
-                    result[k] = field[k].value
-                except (ValueError, AssertionError) as error:
-                    errors[k] = error
+        assert isinstance(data, dict)
+        self._validate(data)
 
-                # Delete data items() after field verification.
-                try:
-                    del data[k]
-                # Ignore possibility when data doesn't have defined field.
-                # In case when field is not required or have default value.
-                except KeyError:
-                    pass
-
-        # After verification, If there's data.keys() left,
-        # it means the field is not defined.
-        for k in data.keys():
-            errors[k] = KeyError('Field is not defined')
-        if errors:
-            raise Exception(errors)
-        self._field = field
-        super().__init__(result)
-
-    def __setitem__(self, k, v):
+    def __setitem__(self, key, value):
         """Verify value before `super().__setitem__`."""
         try:
-            value = self._field[k].query(v).value
+            self._field[key]._validate(value)
+            super().__setitem__(key, value)
         except KeyError:
             raise KeyError('Field is not defined')
 
-        return super().__setitem__(k, value)
+    def _validate(self, data):
+        field = dict()
+        result = dict()
+        error = dict()
+        data = data.copy()
+        for k in self.__dir__():
+            f = self.__getattribute__(k)
+            if not isinstance(f, Field):
+                continue
+            field[k] = f
+            try:
+                result[k] = f._validate(data.get(k)).value
+            except FieldError as e:
+                error[k] = e
+
+            # Delete data items() after field verification.
+            try:
+                del data[k]
+            # Ignore possibility when data doesn't have defined field.
+            # In case when field is not required or have default value.
+            except KeyError:
+                pass
+
+        # After verification, If there's data.keys() left,
+        # it means the field is undefined.
+        for k in data.keys():
+            error[k] = KeyError('Field is not defined')
+        if error:
+            raise ModelError(error)
+        self._field = field
+        super().__init__(result)
 
     def update(self, data):
-        """Modify `update` method to verify data before update."""
-        errors = dict()
-        for k, v in data.items():
-            try:
-                data[k] = self._field[k].query(v).value
-            except ValueError as e:
-                errors[k] = e
-            except KeyError as e:
-                errors[k] = KeyError('Field is not defined')
-        if errors:
-            raise Exception(errors)
-        return super().update(data)
+        self._validate(data)
+        super().update(data)
+        return self
--- dictify/test.py
+++ dictify/test.py
@@ -1,141 +1,153 @@
 import unittest
 import json
 import uuid
-from dictify import Model as BaseModel, Field
+from dictify import Model, Field, FieldError, ModelError
 
 
-class Model(BaseModel):
+class User(Model):
     name = Field().type(str)
     email = Field().type(str)
 
 
-class FieldModel(BaseModel):
-    def uuid4_rule(value):
-        id_ = uuid.UUID(value)
-        assert id_.version == 4
+class MockUp(Model):
 
-    default = Field().default({})
-    required = Field().required()
-    any = Field().anyof([1, 2, 3])
+    def uuid4_rule(field):
+        try:
+            id_ = uuid.UUID(field.value)
+        except AttributeError:
+            raise AttributeError(f"Can't parse value to UUID")
+        assert id_.version == 4, f"Value is UUID v{id_.version}, not v4"
+
+    default = Field(default='default')
+    required = Field(required=True)
     anyof = Field().anyof([1, 2, 3])
-    apply = Field().default(str(uuid.uuid4())).apply(uuid4_rule)
+    verify = Field(default=str(uuid.uuid4())).verify(uuid4_rule)
     length = Field().length(min=2, max=10)
     listof = Field().listof(str)
-    match = Field().match('[0-9]+')
-    number = Field().number(min=0, max=20)
-    range = Field().range(min=0, max=20)
+    match = Field().search('[0-9]+')
+    min = Field().min(0)
+    max = Field().max(10)
     subset = Field().subset([1, 2, 3])
     type = Field().type(str)
     
 
-class TestModel(unittest.TestCase):
+# class TestModel(unittest.TestCase):
 
-    def setUp(self):
-        self.model = Model({'name': 'initial name'})
+#     def setUp(self):
+#         self.model = User({'name': 'initial name'})
 
-    def test_data_unmodified(self):
-        data = {'name': 'test'}
-        Model(data)
-        self.assertDictEqual(data, {'name': 'test'})
+#     def test_data_unmodified(self):
+#         data = {'name': 'test'}
+#         User(data)
+#         self.assertDictEqual(data, {'name': 'test'})
 
-    def test_setitem(self):
-        """Test `__setitem__` for 3 cases:
-        Key Error, ValueError and Success.
-        """
-        with self.assertRaises(ValueError):
-            self.model['name'] = 0
-        with self.assertRaises(KeyError):
-            self.model['age'] = 20
-        self.assertEqual(self.model['name'], 'initial name')
-        self.model['name'] = 'new name'
-        self.assertEqual(self.model['name'], 'new name')
+#     def test_setitem(self):
+#         """Test `__setitem__` for 3 cases:
+#         Key Error, ValueError and Success.
+#         """
+#         data = self.model.copy()
+#         with self.assertRaises(FieldError):
+#             self.model['name'] = 0
+#         with self.assertRaises(KeyError):
+#             self.model['age'] = 20
+#         self.assertDictEqual(
+#             data, self.model,
+#             f"Model's data is the same after error")
+#         self.assertEqual(self.model['name'], 'initial name')
+#         self.model['name'] = 'new name'
+#         self.assertEqual(self.model['name'], 'new name')
 
-    def test_update(self):
-        """Test `update` to raise KeyError and ValueError as Exception."""
-        with self.assertRaises(Exception):
-            self.model.update({'a': 1})
-        with self.assertRaises(Exception):
-            self.model.update({'name': 1})
-
+#     def test_update(self):
+#         """Test `update` to raise KeyError and ValueError as Exception."""
+#         data = self.model.copy()
+#         with self.assertRaises(ModelError):
+#             self.model.update({'a': 1})
+#         with self.assertRaises(ModelError):
+#             self.model.update({'name': 1})
+#         self.assertDictEqual(
+#             data, self.model,
+#             f"Model's data is the same after error")
+        
 
 class TestField(unittest.TestCase):
+
     def setUp(self):
-        self.model = FieldModel({'required': True})
+        self.model = MockUp({'required': True})
 
     def test_anyof(self):
         self.model['anyof'] = 1
-        with self.assertRaises(ValueError):
+        with self.assertRaises(FieldError):
             self.model['anyof'] = 5
 
-    def test_apply(self):
-        self.apply = '11fadebb-3c70-47a9-a3f0-ebf2a3815993'
+    def test_verify(self):
+        self.model['verify'] = '11fadebb-3c70-47a9-a3f0-ebf2a3815993'
 
     def test_default(self):
-        self.assertEqual(self.model['default'], {})
+        self.assertEqual(self.model['default'], 'default')
 
-    def test_length(self):
-        self.model['length'] = 'hello'
-        with self.assertRaises(ValueError):
-            self.model['length'] = 'test-with-lenght-more-than-10'
+#     def test_length(self):
+#         self.model['length'] = 'hello'
+#         with self.assertRaises(ValueError):
+#             self.model['length'] = 'test-with-lenght-more-than-10'
 
-    def test_listof(self):
-        list_of_string = ['ab', 'cd']
-        self.model['listof'] = list_of_string
-        with self.assertRaises(ValueError):
-            self.model['listof'] = [1, 2]
-            self.assertEqual(self.model['listof'], list_of_string)
+#     def test_listof(self):
+#         list_of_string = ['ab', 'cd']
+#         self.model['listof'] = list_of_string
+#         with self.assertRaises(ValueError):
+#             self.model['listof'] = [1, 2]
+#             self.assertEqual(self.model['listof'], list_of_string)
 
-    def test_match(self):
-        self.model['match'] = '0123456789'
-        match = self.model['match']
-        with self.assertRaises(ValueError):
-            self.model['match'] = 'a'
-            self.assertEqual(self.model['match'], match)
+#     def test_match(self):
+#         self.model['match'] = '0123456789'
+#         match = self.model['match']
+#         with self.assertRaises(ValueError):
+#             self.model['match'] = 'a'
+#             self.assertEqual(self.model['match'], match)
 
-    def test_number(self):
-        self.model['number'] = 1
-        with self.assertRaises(ValueError):
-            self.model['number'] = 21
-            self.assertEqual(self.model['number'], 1)
+#     def test_number(self):
+#         self.model['number'] = 1
+#         with self.assertRaises(ValueError):
+#             self.model['number'] = 21
+#             self.assertEqual(self.model['number'], 1)
 
-    def test_range(self):
-        self.model['range'] = 1
-        with self.assertRaises(ValueError):
-            self.model['range'] = 21
-            self.assertEqual(self.model['range'], 1)
+#     def test_range(self):
+#         self.model['range'] = 1
+#         with self.assertRaises(ValueError):
+#             self.model['range'] = 21
+#             self.assertEqual(self.model['range'], 1)
 
-    def test_required(self):
-        required = self.model['required']
-        with self.assertRaises(ValueError):
-            self.model['required'] = None
-            self.assertEqual(self.model['required'], required)
+#     def test_required(self):
+#         required = self.model['required']
+#         with self.assertRaises(ValueError):
+#             self.model['required'] = None
+#             self.assertEqual(self.model['required'], required)
 
-    def test_subset(self):
-        self.model['subset'] = [1, 2, 3]
+#     def test_subset(self):
+#         self.model['subset'] = [1, 2, 3]
 
-    def test_type(self):
-        string = 'test'
-        self.model['type'] = string
-        with self.assertRaises(ValueError):
-            self.model['type'] = 1
-            self.assertEqual(self.model['type'], string)
+#     def test_type(self):
+#         string = 'test'
+#         self.model['type'] = string
+#         with self.assertRaises(ValueError):
+#             self.model['type'] = 1
+#             self.assertEqual(self.model['type'], string)
 
-    def test_json(self):
-        json.dumps(self.model)
+#     def test_json(self):
+#         json.dumps(self.model)
 
-class TestSubClass(unittest.TestCase):
-    def setUp(self):
-        class Content(BaseModel):
-            content_type = Field().type(str)
+# class TestSubClass(unittest.TestCase):
+#     def setUp(self):
+#         class Content(BaseModel):
+#             content_type = Field().type(str)
 
-        class HTML(Content):
-            pass
+#         class HTML(Content):
+#             pass
 
-        self.html = HTML()
+#         self.html = HTML()
 
-    def test_subclass(self):
-        with self.assertRaises(ValueError):
-            self.html['content_type'] = 1
+#     def test_subclass(self):
+#         with self.assertRaises(ValueError):
+#             self.html['content_type'] = 1
 
 
 if __name__ == '__main__':
