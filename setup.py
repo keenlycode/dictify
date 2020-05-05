c148af36f8d98cd1c98516a649e67ebeb0d3d660
Nitipit Nontasuwan <nitipit@gmail.com>
2020-05-05 16:32:12 +0700

    v2.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="dictify",
-    version="2.1.0dev",
+    version="2.1.0",
     author="Nitipit Nontasuwan",
     author_email="nitipit@gmail.com",
     description="Python `dict` and `json` verification for humankind :)",
