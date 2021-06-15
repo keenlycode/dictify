5ac0d263c1a49e8d6f42986a0b2275e78721aeda
Nitipit Nontasuwan <nitipit@gmail.com>
2021-06-15 22:02:50 +0700

    add docs

---
+++ docs/ui/sidebar/sidebar.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("#sidebar-button"),r=document.querySelector("#sidebar");e.addEventListener("click",(e=>{r.show_sidebar()})),console.log(r),r.querySelector('[el="close"]').addEventListener("click",(()=>{r.hide_sidebar()}))}();
+//# sourceMappingURL=sidebar.js.map
