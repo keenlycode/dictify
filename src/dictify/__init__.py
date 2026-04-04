c2d173a6931933ef49c59a8330bb8ca8f0ca1baa
Nitipit Nontasuwan <nitipit@gmail.com>
2026-04-04 22:27:43 +0700

    update

---
+++ src/dictify/__init__.py
@@ -0,0 +1,419 @@
+"""dictify provides lightweight schema and validation helpers for dict data."""
+
+from __future__ import annotations
+
+import re
+from functools import wraps
+from typing import Any, Callable, cast
+
+Validator = Callable[..., Any]
+DefaultFactory = Callable[[], Any]
+DataDict = dict[str, Any]
+
+
+class Function:
+    def __init__(self, func, *args, **kw):
+        self.func = func
+        self.args = args
+        self.kw = kw
+
+    def __call__(self, field, value):
+        return self.func(field, value, *self.args, **self.kw)
+
+    def __repr__(self):
+        return f"{self.func.__name__}{self.args}{self.kw}"
+
+
+def function(func: Validator):
+    """Decorator used in Field class to add methods in validation chain"""
+
+    @wraps(func)
+    def wrapper(self, *args, **kw):
+        # Test default value.
+        if self.default != UNDEF:
+            try:
+                func(self, self.default, *args, **kw)
+            except Exception as error:
+                func_name = getattr(func, "__name__", type(func).__name__)
+                raise Field.DefineError(
+                    f"Field(default={self.default}) conflict with ",
+                    f"{func_name}(*{args}, **{kw})",
+                    error,
+                )
+
+        # Keep function in chain.
+        self._functions.append(Function(func, *args, **kw))
+        return self
+
+    return wrapper
+
+
+class _UNDEF:
+    """Create ```UNDEF`` value"""
+
+    def __repr__(self):
+        return "UNDEF"
+
+
+UNDEF = _UNDEF()
+
+
+class ListOf(list):
+    """Modified list which check it's members instance.
+
+    Parameters
+    ----------
+    value:
+        Value for validation it's type with ``type_``.
+    type_:
+        Type for validation with ``value``
+    """
+
+    class ValueError(Exception):
+        pass
+
+    def __init__(
+        self,
+        values,
+        type_: Any = UNDEF,
+        validate: Validator | None = None,
+    ):
+        if isinstance(type_, type):
+            self.types = (type_,)
+        else:
+            self.types = type_
+
+        self.validate_func = validate
+
+        if self.types[0] == UNDEF:
+            return super().__init__(values)
+
+        for value in values:
+            self._validate(value)
+
+        return super().__init__(values)
+
+    def __setitem__(self, index, value):
+        """Set list value at ``index`` if ``value`` is valid"""
+
+        self._validate(value)
+        return super().__setitem__(index, value)
+
+    def _validate(self, value):
+        if self.types[0] == UNDEF:
+            return
+        if isinstance(value, dict):
+            models = filter(lambda type_: isinstance(type_, Model), self.types)
+            for model_cls in models:
+                try:
+                    model_cls(value)
+                    return
+                except Exception:
+                    pass
+        assert isinstance(value, self.types), (
+            f"'{value}' is not instance of {self.types}"
+        )
+
+        if callable(self.validate_func):
+            self.validate_func(value)
+
+    def append(self, value):
+        """Append object to the list if ``value`` is valid."""
+
+        self._validate(value)
+        return super().append(value)
+
+    def list(self):
+        """Return data as native `list`"""
+
+        data = []
+        for item in self:
+            if isinstance(item, Model):
+                data.append(item.dict())
+            elif isinstance(item, ListOf):
+                data.append(item.list())
+            else:
+                data.append(item)
+        return data
+
+
+class Field:
+    """Create ``Field()`` object which can validate it's value.
+    Can be defined in class ``Model``.
+
+    Examples
+    --------
+    ::
+
+        # Use with validators.
+        field = Field(required=True).anyof(['AM','PM'])
+        field.value = 'AM'
+        field.value = 'A'  # This will raise Field.VerifyError
+
+        # Chained validators.
+        field = Field(default=0).instance(str).search('.*@.*)
+        field.value = 'user@somewhere.com'; # OK
+        field.value = 1  # This will raise Field.VerifyError
+
+    Notes
+    -----
+        As the examples, when defining ``Field()`` validation with
+        it's methods below. The first arguments ``(value)`` can be omitted
+        since it will be put automatically while validating value.
+    ...
+
+    Parameters
+    ----------
+    required: bool=False
+        Required option. If set to ``True``, call ``Field().value`` without
+        assigned value will raise ``Field.RequiredError``
+    default: any=UNDEF
+        Field's default value
+    grant: list
+        Granted values which always valid.
+    """
+
+    class VerifyError(Exception):
+        """Error to be raised if ``Field().value`` doesn't pass validation."""
+
+        pass
+
+    class RequiredError(Exception):
+        """Error to be raised if ``Field(required=True)``
+        but no value provided.
+        """
+
+        pass
+
+    class DefineError(Exception):
+        """Error to be raised when defining ``Field()``."""
+
+        pass
+
+    def __init__(self, required: bool = False, default=UNDEF, grant=None):
+        self.required = required
+        self._default = default
+        if grant is None:
+            grant = []
+        assert isinstance(grant, list)
+        self.grant = grant
+        self._functions = list()
+        self._value = self.default
+
+    @property
+    def default(self):
+        """Field's default value"""
+        if callable(self._default):
+            default_factory = cast(DefaultFactory, self._default)
+            return default_factory()
+        return self._default
+
+    @property
+    def value(self):
+        """``Field()``'s value
+        - Required Field will raise RequiredError if ask for value
+          before assigned.
+        """
+
+        if self.required and self._value == UNDEF:
+            raise Field.RequiredError("Field is required")
+
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        """Set field's value
+        - Verify value by field's functions
+        - Set fields' value if function return value
+        """
+        errors = list()
+        if self.required and value == UNDEF:
+            raise Field.RequiredError("Field is required")
+        if value in self.grant:
+            self._value = value
+            return
+
+        for function in self._functions:
+            try:
+                value_ = function(self, value)
+                if isinstance(value_, (ListOf, Model)):
+                    value = value_
+            except Exception as e:
+                errors.append((function, e))
+        if errors:
+            raise Field.VerifyError(errors)
+        self._value = value
+
+    def reset(self):
+        """Reset ``Field().value`` to default or ``UNDEF``"""
+        self._value = self.default
+
+    @function
+    def instance(self, value, type_: type):
+        """Verify that ``value`` is instance to ``type_``
+
+        ``assert isinstance(value, type_)``
+        """
+        assert isinstance(value, type_), f"{type(value)} is not instance of {type_}"
+
+    @function
+    def listof(
+        self,
+        value: Any,
+        type_: Any = UNDEF,
+        validate: Validator | None = None,
+    ):
+        """Verify list instance"""
+        return ListOf(value, type_, validate)
+
+    @function
+    def match(self, value, re_: str, flags=0):
+        """Match value with regular expression string ``re_``."""
+        assert re.match(re_, value, flags), (
+            f"Matching with re.match('{re_}', '{value}') is None"
+        )
+
+    @function
+    def model(self, value, model_cls: type[Model]):
+        """Verify that value pass ``model_cls`` validation."""
+        return model_cls(value)
+
+    @function
+    def search(self, value, re_: str, flags=0):
+        """Search value with with regular expression string ``re_``."""
+        assert re.search(re_, value, flags), (
+            f"Searching with re.search('{re_}', '{value}') is None"
+        )
+
+    @function
+    def verify(self, value, func, message=None):
+        """Designed to use with ``lambda`` for simple syntax since ``lambda``
+        can't use ``assert`` statement.
+
+        The callable must return ``True`` or ``False``.
+
+        If return ``False``, It will be raised as ``AssertionError``.
+        """
+        assert func(value), message
+
+    @function
+    def func(self, value, fn):
+        """Use callable function to validate value."""
+        fn(value)
+
+
+class Model(dict):
+    """Modified ``dict`` that can defined ``Field`` in it's class."""
+
+    class Error(Exception):
+        """``Exception`` when data doesn't pass ``Model`` validation."""
+
+        pass
+
+    def __init__(self, data: DataDict | None = None, strict: bool = True):
+        if data is None:
+            data = {}
+        assert isinstance(data, dict), "Model initial data should be instance of dict"
+        assert isinstance(strict, bool)
+        data = data.copy()
+        self._field = dict()
+        self._strict = strict
+        for key in self.__dir__():
+            field = self.__getattribute__(key)
+            if not isinstance(field, Field):
+                continue
+            if (field.default != UNDEF) and (key not in data):
+                data[key] = field.value
+            elif field.required:
+                if key not in data:
+                    raise Model.Error(
+                        {key: Field.RequiredError("This field is required")}
+                    )
+            self._field[key] = field
+        data = self._validate(data)
+        super().__init__(data)
+        self.post_validate()
+
+    def __delitem__(self, key):
+        """Delete item but also check for Field's default or required option."""
+
+        if (key not in self._field) and (self._strict is False):
+            super().__delitem__(key)
+            self.post_validate()
+            return
+
+        if self._field[key].default != UNDEF:
+            self[key] = self._field[key].default
+        elif self._field[key].required:
+            raise Model.Error({key: Field.RequiredError("Field is required")})
+        else:
+            super().__delitem__(key)
+        self.post_validate()
+
+    def __setitem__(self, key, value):
+        """Set ``value`` if is valid."""
+
+        error = None
+        if (key not in self._field) and (self._strict is False):
+            super().__setitem__(key, value)
+            self.post_validate()
+            return
+
+        try:
+            self._field[key].value = value
+            super().__setitem__(key, self._field[key].value)
+        except KeyError:
+            error = {key: KeyError("Field is not defined")}
+        except (Field.VerifyError, ListOf.ValueError) as e:
+            error = {key: e}
+        if error:
+            raise Model.Error(error)
+        self.post_validate()
+
+    def _validate(self, data: DataDict):
+        error = dict()
+        for key in data:
+            if (key not in self._field) and (self._strict is False):
+                continue
+
+            try:
+                self._field[key].value = data[key]
+                data[key] = self._field[key].value
+            except KeyError:
+                if self._strict:
+                    error[key] = KeyError("Field is not defined")
+            except Field.VerifyError as e:
+                error[key] = e
+        if error:
+            raise Model.Error(error)
+        return data
+
+    def pop(self, *args, **kw):
+        raise NotImplementedError
+
+    def popitem(self, *args, **kw):
+        raise NotImplementedError
+
+    def post_validate(self):
+        pass
+
+    def update(self, data=None, **kwargs):
+        """Update ``data`` if is valid."""
+        if data is None:
+            data = {}
+        data = dict(data, **kwargs)
+        data = self._validate(data)
+        super().update(data)
+        self.post_validate()
+
+    def dict(self):
+        """Return data as native `dict` and `list`"""
+        data = {}
+        for key, value in self.items():
+            if isinstance(value, Model):
+                data[key] = value.dict()
+            elif isinstance(value, ListOf):
+                data[key] = value.list()
+            else:
+                data[key] = value
+        return data
---
+++ tests/test_dictify.py
@@ -0,0 +1,305 @@
+"""Behavioral tests for the public dictify validation API."""
+
+import json
+import re
+import uuid
+from datetime import datetime, timezone
+from typing import Any, cast
+
+import pytest
+
+from dictify import UNDEF, Field, ListOf, Model
+
+
+def datetime_verify(value):
+    datetime.fromisoformat(value)
+
+
+def uuid4_verify(value):
+    try:
+        id_ = uuid.UUID(value)
+    except AttributeError as exc:
+        raise AttributeError("Can't parse value to UUID") from exc
+    assert id_.version == 4, f"Value is UUID v{id_.version}, not v4"
+
+
+class User(Model):
+    id = Field(default=lambda: uuid.uuid4()).instance(uuid.UUID)
+    name = Field(required=True).instance(str)
+
+
+class Comment(Model):
+    content = Field().instance(str)
+    datetime = Field(default=lambda: datetime.now(timezone.utc)).instance(datetime)
+    user = Field(required=True).instance(User)
+
+
+class Note(Model):
+    title = Field(required=True).instance(str)
+    content = Field().instance(str)
+    datetime = Field(default=lambda: datetime.now(timezone.utc)).instance(datetime)
+    user = Field(required=True).instance(User)
+    comments = Field().listof(Comment)
+
+    def post_validate(self):
+        assert self.get("title") != self.get("content")
+
+
+class UserJSON(Model):
+    id = Field(default=lambda: str(uuid.uuid4())).func(uuid4_verify)
+    name = Field(required=True).instance(str)
+
+
+class CommentJSON(Model):
+    content = Field(required=True).instance(str)
+    datetime = Field(default=lambda: datetime.now(timezone.utc).isoformat()).func(
+        datetime_verify
+    )
+    user = Field(required=True).model(UserJSON)
+
+
+class NoteJSON(Model):
+    title = Field(required=True).instance(str)
+    content = Field().instance(str)
+    datetime = Field(default=lambda: datetime.now(timezone.utc).isoformat()).func(
+        datetime_verify
+    )
+    user = Field(required=True).model(UserJSON)
+    comments = Field().listof(CommentJSON)
+
+
+@pytest.fixture
+def note():
+    return Note(
+        {
+            "title": "Title",
+            "content": "Content",
+            "datetime": datetime.now(timezone.utc),
+            "user": User({"id": uuid.uuid4(), "name": "user1"}),
+            "comments": [
+                Comment(
+                    {
+                        "content": "second user comment",
+                        "user": User({"name": "second user"}),
+                    }
+                ),
+                Comment(
+                    {
+                        "content": "third user comment",
+                        "user": User({"name": "third user"}),
+                    }
+                ),
+            ],
+        }
+    )
+
+
+def test_init(note):
+    with pytest.raises(AssertionError):
+        Note(cast(Any, []))
+
+    with pytest.raises(Model.Error):
+        Note({})
+
+    default_note = Note({"title": "Title", "user": User({"name": "user1"})})
+    assert isinstance(default_note["datetime"], datetime)
+
+    data = {
+        "title": "Title",
+        "content": "Content",
+        "datetime": datetime.now(timezone.utc),
+        "user": User({"id": uuid.uuid4(), "name": "user-1"}),
+    }
+    fresh_note = Note(data)
+    assert fresh_note == data
+    assert note["title"] == "Title"
+
+
+def test_strict(note):
+    strict_note = dict(note)
+    strict_note["undefined_key"] = 1
+    with pytest.raises(Model.Error):
+        Note(strict_note, strict=True)
+
+    del strict_note["undefined_key"]
+    strict_model = Note(strict_note, strict=True)
+    with pytest.raises(Model.Error):
+        strict_model["undefined_key"] = 1
+
+    relaxed_note = dict(note)
+    relaxed_note["undefined_key"] = 1
+    relaxed_model = Note(relaxed_note, strict=False)
+    assert relaxed_model["undefined_key"] == 1
+
+    del relaxed_model["undefined_key"]
+    relaxed_model["undefined_key"] = 1
+    assert relaxed_model["undefined_key"] == 1
+
+
+def test_delitem(note):
+    del note["datetime"]
+    assert isinstance(note["datetime"], datetime)
+
+    title = note["title"]
+    with pytest.raises(Model.Error):
+        del note["title"]
+    assert title == note["title"]
+
+    del note["content"]
+    assert "content" not in note
+
+
+def test_setitem(note):
+    data = note.copy()
+
+    with pytest.raises(Model.Error):
+        note["title"] = 0
+
+    with pytest.raises(Model.Error):
+        note["datetime"] = "today"
+
+    assert note == data
+
+    title = "New Title"
+    note["title"] = title
+    assert note["title"] == title
+
+
+def test_json():
+    note = NoteJSON({"title": "Note JSON", "user": UserJSON({"name": "user-1"})})
+    note_data = json.loads(json.dumps(note))
+    NoteJSON(note_data)
+
+
+def test_update():
+    note = Note({"title": "Title", "user": User({"name": "user example"})})
+    data = note.copy()
+    assert isinstance(data["user"], dict)
+
+    with pytest.raises(Model.Error):
+        note.update({"title": 1})
+
+    with pytest.raises(Model.Error):
+        note.update({"datetime": 1})
+
+    assert data == note
+
+    update = {"title": "New Title", "content": "New Note"}
+    data.update(update)
+    note.update(update)
+    assert data == note
+
+
+def test_field_init():
+    field = Field()
+    assert isinstance(field, Field)
+    assert field._value == UNDEF
+
+    field = Field(required=True, default="default", grant=[None])
+    assert field.required is True
+    assert field._default == "default"
+
+
+def test_field_default():
+    field = Field(default="default")
+    assert field.value == "default"
+    field = Field(default=lambda: datetime.now(timezone.utc))
+    assert isinstance(field.default, datetime)
+
+
+def test_field_value():
+    field = (
+        Field(required=True, grant=[None])
+        .instance(str)
+        .verify(lambda value: len(value) <= 10)
+    )
+
+    with pytest.raises(Field.RequiredError):
+        field.value
+
+    field.value = "test"
+    assert field.value == "test"
+
+    with pytest.raises(Field.VerifyError):
+        field.value = "word-length-more-than-ten"
+
+    assert field.value == "test"
+
+    field.value = None
+    assert field.value is None
+
+
+def test_field_reset():
+    field = Field(default="default")
+    field.value = 1
+    field.reset()
+    assert field.value == "default"
+
+
+def test_field_func():
+    field = Field().func(uuid4_verify)
+    field.value = str(uuid.uuid4())
+    with pytest.raises(Field.VerifyError):
+        field.value = 1
+
+
+def test_field_instance():
+    field = Field().instance(str)
+    field.value = "test"
+    with pytest.raises(Field.VerifyError):
+        field.value = 1
+
+
+def test_field_listof():
+    def less_than_five(value):
+        assert value < 5
+
+    field = Field().listof((int, float), validate=less_than_five)
+    field.value = [1, 2]
+    with pytest.raises(Field.VerifyError):
+        field.value = ["a", "b"]
+
+    with pytest.raises(Field.VerifyError):
+        field.value = [1, 7]
+
+    assert isinstance(field.value, ListOf)
+
+
+def test_field_match():
+    field = Field().match("012", re.I)
+    field.value = "0123456789"
+    with pytest.raises(Field.VerifyError):
+        field.value = "123"
+    assert field.value == "0123456789"
+
+
+def test_field_model():
+    field = Field().model(NoteJSON)
+    note = NoteJSON({"title": "Note", "user": UserJSON({"name": "user-1"})})
+    field.value = note
+
+    note_data = json.loads(json.dumps(note))
+    field.value = note_data
+
+    with pytest.raises(Field.VerifyError):
+        field.value = 1
+
+
+def test_field_search():
+    field = Field().search(r"\w+", re.I)
+    field.value = "0123456789"
+    with pytest.raises(Field.VerifyError):
+        field.value = "?"
+    assert field.value == "0123456789"
+
+
+def test_subclass():
+    class Content(Model):
+        content_type = Field().instance(str)
+
+    class HTML(Content):
+        pass
+
+    html = HTML()
+    with pytest.raises(Model.Error):
+        html["content_type"] = 1
